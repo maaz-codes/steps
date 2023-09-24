b8a9295e04891fe747ce75e224fd8c796ce40560
Maaz Khan <maazkhan7454@gmail.com>
2023-09-24 22:54:35 +0530

    complete steps-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleClosing() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button className="close" onClick={handleClosing}>{isOpen ? 'X' : 'O'}</button>
+      {
+        isOpen && (
+          <div className='steps'>
+            <div className="numbers">
+              <div className={step >= 1 && 'active'}>1</div>
+              <div className={step >= 2 && 'active'}>2</div>
+              <div className={step >= 3 && 'active'}>3</div>
+            </div>
+
+            <div className="message">{`Step ${step} : ${messages[step-1]}`}</div>
+
+            <div className="buttons">
+              <button 
+              style={{backgroundColor: "#7950f2", color: "#fff"}}
+              onClick={() => {if (step > 1) setStep(step-1)}}>
+                <span>Previous</span>
+              </button>
+              <button 
+              style={{backgroundColor: "#7950f2", color: "#fff"}}
+              onClick={() => {if (step < 3) setStep(step+1)}}>
+                <span>Next</span>
+              </button>
+            </div>
+          </div>
+        )
+      }
+    </>
+  )
 }
 
-export default App;
+export default App
